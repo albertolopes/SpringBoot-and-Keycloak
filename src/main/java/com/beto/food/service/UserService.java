ac6423875160b8e5a4c9ad9c1257d8f5d230dbdd
Alberto Lopes <alberto_llopes@hotmail.com>
2021-01-02 16:40:18 -0300

    Upload Authenticate

--- src/main/java/com/beto/food/service/UserService.java
+++ src/main/java/com/beto/food/service/UserService.java
@@ -7,6 +7,7 @@ import com.beto.food.exception.ObjectAlreadyExistsException;
 import com.beto.food.exception.ObjectNotFoundException;
 import com.beto.food.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.representations.AccessToken;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,7 +21,6 @@ public class UserService {
     private final AuthenticationConfig authenticationConfig;
 
     public User salvar(User user) {
-        authenticationConfig.getLoggedUser();
         validateUserInsert(user);
         user.setId(null);
         return repository.save(user);
